3238e1c0d3028578f13e1ddcb5302bf5cfe78740
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-21 23:29:52 -0300

    Add viewModel for GuestForm

---
+++ app/src/main/java/com/example/guests/view/GuestFormActivity.kt
@@ -0,0 +1,32 @@
+package com.example.guests
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_guest_form.*
+
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mViewModel: GuestFormViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guest_form)
+
+        mViewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        button_save.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if (id == R.id.button_save) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/guests/viewModel/GuestFormViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.guests.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class GuestFormViewModel : ViewModel() {
+
+}
