f1710d20797184e0b027729fbf094882efdc13e0
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-22 17:46:10 -0300

    Add observe function

--- app/src/main/java/com/example/guests/view/GuestFormActivity.kt
+++ app/src/main/java/com/example/guests/view/GuestFormActivity.kt
@@ -3,6 +3,8 @@ package com.example.guests.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.guests.viewModel.GuestFormViewModel
 import com.example.guests.R
@@ -19,16 +21,31 @@ class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
         mViewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
 
         setListeners()
-    }
-
-    private fun setListeners() {
-        button_save.setOnClickListener(this)
+        observe()
     }
 
     override fun onClick(view: View) {
         val id = view.id
         if (id == R.id.button_save) {
 
+            val name = edit_name.text.toString()
+            val presence = radio_presence.isChecked
+
+            mViewModel.save(name, presence)
         }
+    }
+
+    private fun observe() {
+        mViewModel.saveGuest.observe(this, Observer {
+            if (it) {
+                Toast.makeText(applicationContext, "Sucesso!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Falha!", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun setListeners() {
+        button_save.setOnClickListener(this)
     }
 }
