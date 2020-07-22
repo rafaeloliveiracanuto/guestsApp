aba8817554ce3cf3e6a939af784d2e34aec9eb11
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-22 17:48:57 -0300

    Fix GuestRepository instance

--- app/src/main/java/com/example/guests/viewModel/GuestFormViewModel.kt
+++ app/src/main/java/com/example/guests/viewModel/GuestFormViewModel.kt
@@ -1,7 +1,24 @@
 package com.example.guests.viewModel
 
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.guests.service.model.GuestModel
+import com.example.guests.service.repository.GuestRepository
 
-class GuestFormViewModel : ViewModel() {
+class GuestFormViewModel(application: Application) : AndroidViewModel(application) {
+    private val mContext = application.applicationContext
+    private val mGuestRepository: GuestRepository = GuestRepository.getInstance(mContext)
+
+    private var mSaveGuest = MutableLiveData<Boolean>()
+    val saveGuest: LiveData<Boolean> = mSaveGuest
+
+    fun save(name: String, presence: Boolean) {
+        val guest = GuestModel(name, presence)
+        mGuestRepository.save(guest)
+    }
 
 }
