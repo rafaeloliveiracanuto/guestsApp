2ba5f9f3d0fe6bb6452f727ba62c074b3e745a6c
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-23 12:04:16 -0300

    Implement inheritance's class members

--- app/src/main/java/com/example/guests/view/adapter/GuestAdapter.kt
+++ app/src/main/java/com/example/guests/view/adapter/GuestAdapter.kt
@@ -2,18 +2,20 @@ package com.example.guests.view.adapter
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.guests.view.viewHolder.GuestViewHolder
 
-class GuestAdapter : RecyclerView.Adapter<> {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ??? {
-
+class GuestAdapter : RecyclerView.Adapter<GuestViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GuestViewHolder {
+        TODO("Not yet implemented")
     }
 
     override fun getItemCount(): Int {
-
+        TODO("Not yet implemented")
     }
 
-    override fun onBindViewHolder(holder: ???, position: Int) {
-
+    override fun onBindViewHolder(holder: GuestViewHolder, position: Int) {
+        TODO("Not yet implemented")
     }
+
 
 }
