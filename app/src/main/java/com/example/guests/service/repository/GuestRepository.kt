a9814d201a5637a95d46087e86bf670b58229d8f
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-22 22:41:53 -0300

    Add delete function in GuestRepository

--- app/src/main/java/com/example/guests/service/repository/GuestRepository.kt
+++ app/src/main/java/com/example/guests/service/repository/GuestRepository.kt
@@ -51,7 +51,7 @@ class GuestRepository private constructor(context: Context){
         }
     }
 
-    fun update(guest: GuestModel): Boolean {
+    fun update(guest: GuestModel) : Boolean {
         return try {
             val db = mGuestDataBaseHelper.writableDatabase
 
@@ -69,8 +69,18 @@ class GuestRepository private constructor(context: Context){
         }
     }
 
-    fun delete(guest: GuestModel) {
+    fun delete(id: Int) : Boolean {
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
 
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(id.toString())
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args)
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 
 }
