c05eb205e7988512488518b6e19feb74a883047f
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-23 14:56:46 -0300

    Add bind function to GuestViewHolder

--- app/src/main/java/com/example/guests/view/viewHolder/GuestViewHolder.kt
+++ app/src/main/java/com/example/guests/view/viewHolder/GuestViewHolder.kt
@@ -1,8 +1,17 @@
 package com.example.guests.view.viewHolder
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.guests.R
+import com.example.guests.service.model.GuestModel
+import kotlinx.android.synthetic.main.row_guest.view.*
 
 class GuestViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(guest: GuestModel) {
+        val textName = itemView.findViewById<TextView>(R.id.text_name)
+        textName.text = guest.name
+    }
 
 }
