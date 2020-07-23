c3b749480c51a188d94194a63c29736f0998bfe9
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-23 14:55:06 -0300

    Add observer

--- app/src/main/java/com/example/guests/view/AllGuestsFragment.kt
+++ app/src/main/java/com/example/guests/view/AllGuestsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -13,11 +12,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.guests.R
 import com.example.guests.view.adapter.GuestAdapter
 import com.example.guests.viewModel.AllGuestsViewModel
-import kotlinx.android.synthetic.main.fragment_all.*
 
 class AllGuestsFragment : Fragment() {
 
     private lateinit var allGuestsViewModel: AllGuestsViewModel
+
+    private val mAdapter: GuestAdapter = GuestAdapter()
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -34,8 +34,18 @@ class AllGuestsFragment : Fragment() {
 
         recycler.layoutManager = LinearLayoutManager(context)
 
-        recycler.adapter = GuestAdapter()
+        recycler.adapter = mAdapter
+
+        observer()
+
+        allGuestsViewModel.load()
 
         return root
+    }
+
+    private fun observer() {
+        allGuestsViewModel.guestList.observe(viewLifecycleOwner, Observer {
+            mAdapter.updateGuests(it)
+        })
     }
 }
