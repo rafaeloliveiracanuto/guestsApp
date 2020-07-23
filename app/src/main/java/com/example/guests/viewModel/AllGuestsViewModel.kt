c32f8689e77547b5f4e66a1317ce59035f4579ff
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-23 14:57:33 -0300

    Fix few things and add load function

--- app/src/main/java/com/example/guests/viewModel/AllGuestsViewModel.kt
+++ app/src/main/java/com/example/guests/viewModel/AllGuestsViewModel.kt
@@ -1,13 +1,22 @@
 package com.example.guests.viewModel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.guests.service.model.GuestModel
+import com.example.guests.service.repository.GuestRepository
 
-class AllGuestsViewModel : ViewModel() {
+class AllGuestsViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val mGuestsRepository = GuestRepository.getInstance(application.applicationContext)
+
+    private val mGuestList = MutableLiveData<List<GuestModel>>()
+
+    val guestList: LiveData<List<GuestModel>> = mGuestList
+
+    fun load() {
+        mGuestList.value = mGuestsRepository.getAll()
     }
-    val text: LiveData<String> = _text
 }
