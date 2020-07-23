2c77f7767a7ea7c24685afca8e3f5f153e8acd1e
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-23 11:50:00 -0300

    Create GuestAdapter

---
+++ app/src/main/java/com/example/guests/view/adapter/GuestAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.guests.view.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class GuestAdapter : RecyclerView.Adapter<> {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ??? {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: ???, position: Int) {
+
+    }
+
+}
