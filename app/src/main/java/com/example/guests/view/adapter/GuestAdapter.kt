434c6ae1dfb40bca34089246a7edcc0a55d50ed7
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-24 09:03:16 -0300

    Add attachListener

--- app/src/main/java/com/example/guests/view/adapter/GuestAdapter.kt
+++ app/src/main/java/com/example/guests/view/adapter/GuestAdapter.kt
@@ -5,15 +5,17 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.guests.R
 import com.example.guests.service.model.GuestModel
+import com.example.guests.view.listener.GuestListener
 import com.example.guests.view.viewHolder.GuestViewHolder
 
 class GuestAdapter : RecyclerView.Adapter<GuestViewHolder>() {
 
     private var mGuestList: List<GuestModel> = arrayListOf()
+    private lateinit var mListener: GuestListener
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GuestViewHolder {
         val item = LayoutInflater.from(parent.context).inflate(R.layout.row_guest, parent, false)
-        return GuestViewHolder(item)
+        return GuestViewHolder(item, mListener)
     }
 
     override fun getItemCount(): Int {
@@ -27,6 +29,10 @@ class GuestAdapter : RecyclerView.Adapter<GuestViewHolder>() {
     fun updateGuests(list: List<GuestModel>) {
         mGuestList = list
         notifyDataSetChanged()
+    }
+
+    fun attachListener(listener: GuestListener) {
+        mListener = listener
     }
 
 
