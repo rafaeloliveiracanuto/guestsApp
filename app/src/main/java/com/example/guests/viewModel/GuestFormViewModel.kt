f03479024c7bec4c980d8f1305528e5e0932efa4
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-24 09:22:25 -0300

    Finish update option

--- app/src/main/java/com/example/guests/viewModel/GuestFormViewModel.kt
+++ app/src/main/java/com/example/guests/viewModel/GuestFormViewModel.kt
@@ -19,10 +19,15 @@ class GuestFormViewModel(application: Application) : AndroidViewModel(applicatio
     private var mGuest = MutableLiveData<GuestModel>()
     val guest: LiveData<GuestModel> = mGuest
 
-    fun save(name: String, presence: Boolean) {
-        val guest = GuestModel(name = name, presence = presence)
+    fun save(id: Int, name: String, presence: Boolean) {
+        val guest = GuestModel(id, name, presence)
 
-        mSaveGuest.value = mGuestRepository.save(guest)
+        if (id == 0) {
+            mSaveGuest.value = mGuestRepository.save(guest)
+        } else {
+            mSaveGuest.value = mGuestRepository.update(guest)
+        }
+
     }
 
     fun load(id: Int) {
