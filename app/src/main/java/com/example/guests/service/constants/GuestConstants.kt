b890d4d9021a2cc8e285ef6f9b2e21a4d9968eda
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-24 17:32:59 -0300

    Add filter to guest constants

--- app/src/main/java/com/example/guests/service/constants/GuestConstants.kt
+++ app/src/main/java/com/example/guests/service/constants/GuestConstants.kt
@@ -4,4 +4,10 @@ class GuestConstants private constructor(){
     companion object {
         const val GUEST_ID = "guestID"
     }
+
+    object FILTER{
+        const val EMPTY = 0
+        const val PRESENTS = 1
+        const val ABSENTS = 2
+    }
 }
