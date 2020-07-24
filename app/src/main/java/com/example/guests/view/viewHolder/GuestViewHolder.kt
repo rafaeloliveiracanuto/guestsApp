ffe73a04926834309fab1ecebece38cb0e0315ca
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-24 09:01:31 -0300

    Add setOnClickListener for textName

--- app/src/main/java/com/example/guests/view/viewHolder/GuestViewHolder.kt
+++ app/src/main/java/com/example/guests/view/viewHolder/GuestViewHolder.kt
@@ -5,13 +5,18 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.guests.R
 import com.example.guests.service.model.GuestModel
+import com.example.guests.view.listener.GuestListener
 import kotlinx.android.synthetic.main.row_guest.view.*
 
-class GuestViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class GuestViewHolder(itemView: View, private val listener: GuestListener) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(guest: GuestModel) {
         val textName = itemView.findViewById<TextView>(R.id.text_name)
         textName.text = guest.name
+
+        textName.setOnClickListener {
+            listener.onClick(guest.id)
+        }
     }
 
 }
